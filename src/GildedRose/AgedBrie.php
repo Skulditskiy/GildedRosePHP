9fb844a6b6e7f3beb2036cefac0141b050422ce8
Yevgeniy Skulditskiy <e@virtech.kz>
2017-02-23 03:59:52 +0600

    - fixes in logic to conform story

---
+++ src/GildedRose/AgedBrie.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace GildedRose;
+
+class AgedBrie extends Item
+{
+    public function UpdateQuality()
+    {
+
+    }
+}
--- src/GildedRose/Program.php
+++ src/GildedRose/Program.php
@@ -103,42 +103,37 @@ class Program
     {
         switch ($item->name) {
             case 'Aged Brie':
+                $item->sellIn = $item->sellIn - 1;
                 if ($item->quality < 50) {
                     $item->quality++;
                 }
-                $item->sellIn = $item->sellIn - 1;
-                if ($item->sellIn < 0) {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
+                if ($item->sellIn < 0 && $item->quality < 50) {
+                    $item->quality++;
                 }
                 break;
             case 'Backstage passes to a TAFKAL80ETC concert':
-                if ($item->quality < 50) {
-                    $item->quality++;
+                $item->sellIn = $item->sellIn--;
+                if ($item->sellIn <= 10 && $item->quality < 49) {
+                    $item->quality += 2;
                 }
-                if ($item->sellIn < 11 && $item->quality < 50) {
-                    $item->quality = $item->quality + 1;
+                if ($item->sellIn <= 5 && $item->quality < 50) {
+                    $item->quality = $item->quality++;
                 }
-                if ($item->sellIn < 6 && $item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                }
-                $item->sellIn = $item->sellIn - 1;
                 if ($item->sellIn < 0) {
                     $item->quality = 0;
                 }
                 break;
             case 'Sulfuras, Hand of Ragnaros':
-                if ($item->quality < 50) {
+                if ($item->quality < 80) {
                     $item->quality++;
                 }
                 break;
             default:
+                $item->sellIn--;
                 if ($item->quality > 0) {
                     $item->quality--;
                 }
-                $item->sellIn = $item->sellIn - 1;
-                if ($item->sellIn < 0) {
+                if ($item->sellIn < 0 && $item->quality > 0) {
                     $item->quality--;
                 }
                 break;
